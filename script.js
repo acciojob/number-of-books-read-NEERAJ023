fe32369be159b6bc0a9ff953067d307b5268bf42
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-10-18 12:54:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,8 +18,18 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count=0;
+	
+	for(let i=0 ; i<library.length; i++){
+		library.i.readingStatus==true
+		if (library.i.readingStatus) {
+			count++;
+		}
+	}
+	return count;
 };
 
 // Do not change the code below
 
 alert(numberOfBooksRead());
+
